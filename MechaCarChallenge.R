c4ceb233329c1edabe643489bdb94e409cb440f1
agaffney-hub <70240501+agaffney-hub@users.noreply.github.com>
2022-09-07 23:06:40 -0700

    Final Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,24 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #summary of linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, Mecha_data))
+
+##DELIVERABLE 2
+
+#importing csv as dataframe
+SusCoil <- read.csv('Suspension_Coil.csv')
+
+#Make table of Suspension Coil
+CoilSummary <- SusCoil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+#A lot summary of data from table
+lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+## DELIVERABLE 3
+
+# T Test for PSI versus population of 1,500 pounds/sq. inch
+t.test(SusCoil$PSI, mu=1500)
+
+#T Tests for each lot 
+t.test(subset(SusCoil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(SusCoil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(SusCoil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
